15d0d16d5e086c88fcc5980cdd5e31e9cdb3b4fa
kar834 <123202320+kar834@users.noreply.github.com>
2023-01-20 14:59:17 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
+  cout<<"---------------------------------------"<<endl;
+  cout<< "Kevin Romero" <<endl;
+  cout<< "Computer Science 1"<<endl;
+  cout<< "Lab1" <<endl;
+  cout<< "January 20, 2023"<<endl;
+  cout<< "--------------------------------------"<<endl;
+  cout << "Orange county community college" << endl;
+  cout<<"lab1"<<endl;
+  int x;
+  int y;
+  x= 20;
+  //cout<<"content of X right after initialization: " << x << endl;
+  y= 5;
+  cout<<"X + Y = " << x + y<<endl;
+  //cout<<"content of y right before program exit: " << y << endl;
+
+  return 0;
+}
